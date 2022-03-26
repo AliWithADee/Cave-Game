2a7368159437cc4e4ecf814a140c2a76ba9218ca
AliDee <ali.dee1410@gmail.com>
2022-03-26 11:58:48 +0000

    Melee attacks

--- Player.gd
+++ Player.gd
@@ -3,9 +3,15 @@ extends KinematicBody2D
 export var speed = 100
 
 onready var anim: AnimationPlayer = $AnimationPlayer
+onready var hurt_boxes = $HurtBoxes
 
 onready var facing = Vector2.DOWN
 onready var velocity = Vector2.ZERO
+
+var attacking = false
+
+func _ready():
+	anim.connect("animation_finished", self, "on_animation_finished")
 
 func _unhandled_input(event):
 	if event.is_action_pressed("melee"):
@@ -28,26 +34,40 @@ func _process(delta):
 	if velocity != Vector2.ZERO:
 		facing = velocity.normalized()
 	
-	if velocity.x > 0: anim.play("Walk_Right")
-	elif velocity.x < 0: anim.play("Walk_Left")
-	elif velocity.y > 0: anim.play("Walk_Down")
-	elif velocity.y < 0: anim.play("Walk_Up")
-	else:
-		if facing.x > 0: anim.play("Idle_Right")
-		elif facing.x < 0: anim.play("Idle_Left")
-		elif facing.y < 0: anim.play("Idle_Up")
-		else: anim.play("Idle_Down")
+	if not attacking:
+		if velocity.x > 0: anim.play("Walk_Right")
+		elif velocity.x < 0: anim.play("Walk_Left")
+		elif velocity.y > 0: anim.play("Walk_Down")
+		elif velocity.y < 0: anim.play("Walk_Up")
+		else:
+			if facing.x > 0: anim.play("Idle_Right")
+			elif facing.x < 0: anim.play("Idle_Left")
+			elif facing.y < 0: anim.play("Idle_Up")
+			else: anim.play("Idle_Down")
 	
 	# Move the player using velocity vector, then set velocity to the result of us attempting to move.
 	velocity = move_and_slide(velocity)
 	
 func melee_attack():
 	if facing.x > 0:
-		print("attack right")
+		anim.play("Melee_Right")
+		hurt_boxes.enable_hurt_box("MeleeRight")
+		hurt_boxes.enable_hurt_box("MineRight")
 	elif facing.x < 0:
-		print("attack left")
+		anim.play("Melee_Left")
+		hurt_boxes.enable_hurt_box("MeleeLeft")
+		hurt_boxes.enable_hurt_box("MineLeft")
 	elif facing.y < 0:
-		print("attack up")
+		anim.play("Melee_Up")
+		hurt_boxes.enable_hurt_box("MeleeUp")
+		hurt_boxes.enable_hurt_box("MineUp")
 	else:
-		print("attack down")
-	
+		anim.play("Melee_Down")
+		hurt_boxes.enable_hurt_box("MeleeDown")
+		hurt_boxes.enable_hurt_box("MineDown")
+	attacking = true
+
+func on_animation_finished(animation):
+	if "Melee" in animation or "Revolver" in animation or "Dynamite" in animation:
+		$HurtBoxes.disable_hurt_boxes()
+		attacking = false
